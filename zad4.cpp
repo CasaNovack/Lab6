212fa8db4faa4d02d91cf74d59b56a84e8158f39
CasaNovack <92743291+CasaNovack@users.noreply.github.com>
2021-12-21 20:39:22 +0100

    Add files via upload

---
+++ zad4.cpp
@@ -0,0 +1,39 @@
+﻿// zad4.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include<time.h>
+using namespace std;
+
+
+int main()
+{
+	srand (time (NULL) );
+	int tab[10];
+	for (int i = 0; i < 10; i++)
+	{
+		tab[i] = rand()%78 -23;
+		cout << tab[i]<<endl; 
+	}
+	cout << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		if (tab[0] > tab[i])
+		{
+			tab[0] = tab[i];
+		}
+	}
+	cout << tab[0];
+}
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
